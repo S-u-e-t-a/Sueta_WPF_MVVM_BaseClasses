a75b89a1a3c573435ff2fc8d382302a41c6721ea
Anton <anton.gusevv.01@gmail.com>
2021-10-30 23:41:47 +0300

    добавлены базовые классы

---
+++ WPF_MVVM_Classes/WPF_MVVM_Classes/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_MVVM_Classes
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        protected ViewModelBase() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose() { }
+    }
+}
